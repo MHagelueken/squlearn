92fb8150c6ea1217198933abfc29a50e3482f8a4
ManuelHagelueken <manuel.hagelueken@ipa.fraunhofer.de>
2023-12-08 16:00:22 +0100

    test

--- src/squlearn/qnn/loss.py
+++ src/squlearn/qnn/loss.py
@@ -475,7 +475,6 @@ class SquaredLoss(LossBase):
         if multiple_output:
             d_p = 2.0 * np.einsum("ij,ijk->k", weighted_diff, value_dict["dfdp"])
         else:
-            print([i[0] for i in value_dict["dfdp"]])
             d_p = 2.0 * np.einsum("j,jk->k", weighted_diff, [i[0] for i in value_dict["dfdp"]])
 
         # Extra code for the cost operator derivatives
@@ -485,7 +484,6 @@ class SquaredLoss(LossBase):
         if multiple_output:
             d_op = 2.0 * np.einsum("ij,ijk->k", weighted_diff, value_dict["dfdop"])
         else:
-            print([i[0] for i in value_dict["dfdop"]])
             d_op = 2.0 * np.einsum("j,jk->k", weighted_diff, [i[0] for i in value_dict["dfdop"]])
         return d_p, d_op
 
